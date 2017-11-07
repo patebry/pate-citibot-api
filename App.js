1f5ccad9152ddd3d2e002e780377db9f40a83083
Pate Byant <patebryant@Pates-MacBook-Pro.local>
2017-11-07 16:31:24 -0500

    readme done and small changes to data structure

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const dal = require('./dal')
-const port = process.env.PORT || 4000
+const port = 4000
 const HTTPError = require('node-http-error')
 const bodyParser = require('body-parser')
+
 const cors = require('cors')
+app.use(cors())
+
 const { pathOr, keys, pick, head, assoc } = require('ramda')
 const checkRequiredFields = require('./lib/check-required-fields')
 
 app.use(bodyParser.json())
-app.use(cors({ credentials: true }))
 
 const { pluck, map, replace, compose } = require('ramda')
 const twilio = require('twilio')(
@@ -34,40 +36,39 @@ const cityReqFieldCheck = checkRequiredFields([
 app.post('/cities', function(req, res, next) {
 	var body = pathOr(null, ['body'], req)
 	const reqFieldsCheckResults = cityReqFieldCheck(body)
-	console.log('body.name', body.name)
+	body = pick(
+		['name', 'description', 'sites', 'content', '_id', 'trello'],
+		body
+	)
+	body = assoc(
+		'bot',
+		{
+			name: 'citi',
+			alias: 'dev'
+		},
+		body
+	)
 	twilio.incomingPhoneNumbers.create({
 		phoneNumber: body._id
 	})
-	/*
-	,
-	function(err, purchasedNumber) {
-		console.log('purchasedNumber', purchasedNumber)
-	}
-	*/
-	// call trello to create a new board of city name
-	// also create a inbound/complete list on trello
 	setupTrello(body.name, ['inbound', 'complete']).then(res => {
 		console.log('res', res)
 		;(body.trello = {
 			app: process.env.TRELLO_KEY,
 			token: process.env.TRELLO_TOKEN,
 			list: res.id
-		}), pathOr(0, ['length'], reqFieldsCheckResults) > 0
-			? next(
-					new HTTPError(400, 'Bad request.  Missing required field', {
-						missingFields: reqFieldsCheckResults
-					})
-				)
-			: dal.createCity(
-					pick(
-						['name', 'description', 'sites', 'content', '_id', 'trello'],
-						body
-					),
-					(err, result) => {
+		}),
+			pathOr(0, ['length'], reqFieldsCheckResults) > 0
+				? next(
+						console.log(body),
+						new HTTPError(400, 'Bad request.  Missing required field', {
+							missingFields: reqFieldsCheckResults
+						})
+					)
+				: dal.createCity(body, (err, result) => {
 						if (err) next(new HTTPError(err.status, err.message, err))
 						res.status(201).send(result)
-					}
-				)
+					})
 	})
 })
 
@@ -135,7 +136,7 @@ app.delete('/cities/:id', function(req, res, next) {
 
 //List Cities
 app.get('/cities', (req, res, next) => {
-	const limit = Number(pathOr(10, ['query', 'limit'], req))
+	const limit = Number(pathOr(100, ['query', 'limit'], req))
 	const filter = pathOr(null, ['query', 'filter'], req)
 	const lastItem = pathOr(null, ['query', 'lastItem'], req)
 
@@ -146,7 +147,7 @@ app.get('/cities', (req, res, next) => {
 })
 
 // Get numbers
-app.get('/numbers/:areaCode', (req, res) => {
+app.get('/numbers/:areaCode', (req, res, next) => {
 	var areaCode = req.params.areaCode
 	console.log('areaCode', areaCode)
 
@@ -154,6 +155,7 @@ app.get('/numbers/:areaCode', (req, res) => {
 		areaCode: areaCode
 	}, function(err, data) {
 		if (err) {
+			console.log('error', err)
 			return res.send(err)
 		}
 		// map(prop('phone'), data) === pluck('prop', data)
--- dal.js
+++ dal.js
@@ -1,14 +1,23 @@
 const dalHelper = require('./lib/dal-helper')
 const PouchDB = require('pouchdb')
 PouchDB.plugin(require('pouchdb-find'))
-const { prop, assoc, head, last, split } = require('ramda')
+const { prop, assoc, head, last, split, trim } = require('ramda')
 
-const createCity = (body, callback) => dalHelper.create(body, callback)
+const createCity = (body, callback) =>
+	dalHelper.create(createFormat(body), callback)
 const showCity = (id, callback) => dalHelper.read(id, callback)
 const updateCity = (body, callback) => dalHelper.update(body, callback)
 const deleteCity = (id, callback) => dalHelper.deleteDoc(id, callback)
-const listCities = (lastItem, filter, limit, callback) =>
+const listCities = (lastItem, filter, limit, callback) => {
 	dalHelper.list(lastItem, filter, limit, callback)
+}
+
+function createFormat(body) {
+	body._id = trim(body._id)
+	console.log(body)
+	return body
+}
+
 const dal = {
 	createCity,
 	showCity,
